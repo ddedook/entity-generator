842e18a8b435a4baf7b2eda7551373a60c55ba02
harris.ding <harris.ding@izeniu.com>
2016-12-19 22:47:47 +0800

    update

--- src/main/java/com/shzisg/generator/processor/DatabaseProcessor.java
+++ src/main/java/com/shzisg/generator/processor/DatabaseProcessor.java
@@ -4,27 +4,24 @@ import com.shzisg.generator.output.DomainDefine;
 import com.shzisg.generator.output.EntityDefine;
 
 public class DatabaseProcessor implements Processor {
-
-    @Override
-    public DomainDefine process(DomainDefine domainDefine) {
-        for (EntityDefine entityDefine : domainDefine.getEntities()) {
-            entityDefine.addImport("javax.persistence.*");
-            entityDefine.addDecorator("@Entity");
-            entityDefine.addDecorator("@Table(name = \"" + entityDefine.getTableConfig().getName() + "\")");
-            entityDefine.getProperties().forEach(props -> {
-                if (props.getColumnConfig().isPrimary()) {
-                    props.addDecorator("@Id")
-                            .addDecorator("@Column(name = \"" + props.getName() + "\")")
-                            .addDecorator("@GeneratedValue(generator = \"generator\")")
-                            .addDecorator("@GenericGenerator(name = \"generator\", strategy = \"uuid\")");
-                } else if (!props.getColumnConfig().isExtend()) {
-                    props.addDecorator("@Basic")
-                            .addDecorator("@Column(name = \"" + props.getColumnConfig().getName() + "\")");
-                } else {
-                    props.addDecorator("@Transient");
-                }
-            });
+  
+  @Override
+  public DomainDefine process(DomainDefine domainDefine) {
+    for (EntityDefine entityDefine : domainDefine.getEntities()) {
+      entityDefine.addImport("javax.persistence.*");
+      entityDefine.addDecorator("@Table(name = \"" + entityDefine.getTableConfig().getName() + "\")");
+      entityDefine.getProperties().forEach(props -> {
+        if (props.getColumnConfig().isPrimary()) {
+          props.addDecorator("@Id")
+            .addDecorator("@Column(name = \"" + props.getName() + "\")")
+            .addDecorator("@GeneratedValue(generator = \"uuid\")");
+        } else if (!props.getColumnConfig().isExtend()) {
+          props.addDecorator("@Column(name = \"" + props.getColumnConfig().getName() + "\")");
+        } else {
+          props.addDecorator("@Transient");
         }
-        return domainDefine;
+      });
     }
+    return domainDefine;
+  }
 }
